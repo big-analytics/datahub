3654e081fac081de3b9ac02b81e72e0d5241df5f
MARIN Gratiel Marius <gratiel-marius.marin@eurocontrol.int>
2022-02-18 09:19:41 +0000

    added git commands

--- run_first.sh
+++ run_first.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+git remote add origin_datahub https://github.com/linkedin/datahub.git
+git pull origin_datahub master
+git push origin
 pip3 install --upgrade acryl-datahub -i http://mnexus001/repository/pypi-pypi/simple --trusted-host mnexus001
